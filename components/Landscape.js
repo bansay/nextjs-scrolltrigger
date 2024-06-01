05dc51c7fe07c97df6874466236dd440c6a971c1
Brittany Ansay <brittansay@bansay.local>
2024-06-01 14:31:13 -0600

    remove not used stuff

--- components/Landscape.js
+++ components/Landscape.js
@@ -24,7 +24,6 @@ const Landscape = () => {
     const landscape_sky = document.querySelectorAll(".baa-sky img");    
     const landscape_mtns_bg = document.querySelectorAll(".baa-bg img");
     const landscape_mtns_fg = document.querySelectorAll(".baa-fg img");  
-    const landscape_cloud = document.querySelectorAll(".baa-cloud .cloud-inner"); 
 
     let foreground_timeline = gsap.timeline();
     let landscape_timeline = gsap.timeline();
@@ -37,32 +36,7 @@ const Landscape = () => {
       start: "top top",
       markers: false,
       end: "300px",
-    });
-  
-    const bgTrigger = ScrollTrigger.create({
-      trigger: landscape_wrapper,
-      pin: false,
-      start: "top top",
-      markers: false,
-      end: "300px",
-    }); 
-    
-    const fgTrigger = ScrollTrigger.create({
-      trigger: landscape_wrapper,
-      pin: false,
-      start: "top top",
-      markers: false,
-      end: "300px",
     });  
-    
-    const cloudTrigger = ScrollTrigger.create({
-      trigger: landscape_wrapper,
-      pin: landscape_cloud,
-      start: "top top",
-      markers: false,
-      end: "200px",
-    }); 
-
     
     mm.add("(min-width: 768px)", () => {
       landscape_timeline.to(landscape_sky,{
@@ -74,28 +48,20 @@ const Landscape = () => {
         }
       });       
     });
-
-    foreground_timeline.to(landscape_cloud,{
-      opacity:0,      
-      scrollTrigger:{
-        start:() => cloudTrigger?.start,
-        end:() => cloudTrigger?.end,
-        scrub:1,
-      }
-    }).to(landscape_mtns_bg,{
+    foreground_timeline.to(landscape_mtns_bg,{
       y:"390px",
       x:"44px",
       scrollTrigger:{
-        start:() => bgTrigger?.start,
-        end:() => bgTrigger?.end,
+        start:() => skyTrigger?.start,
+        end:() => skyTrigger?.end,
         scrub:1,
       }
     }).to(landscape_mtns_fg,{
       y:"400px",
       x:"-44px",
       scrollTrigger:{
-        start:() => fgTrigger?.start,
-        end:() => fgTrigger?.end,
+        start:() => skyTrigger?.start,
+        end:() => skyTrigger?.end,
         scrub:1,
       }
     });
@@ -103,13 +69,7 @@ const Landscape = () => {
   }); 
 
   return (    
-    <div className="wrapper">
-      <div className="baa-cloud">
-        <div className="cloud-inner bg-white text-black text-center">          
-          <h1 className="text-3xl font-bold mb-1">Hi!</h1>
-          <p className="text-sm">Thanks for checking out my source code. If you have any feedback please feel free to reach out. Cheers!</p>                    
-        </div>
-      </div>            
+    <div className="wrapper">           
       <div className="baa-sky baa-inner">        
         <Image
           src="/viafoci-scrolltrigger-sky-dark.jpg" 
